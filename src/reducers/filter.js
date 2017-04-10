776fd3e384f58e87a3f57467dce3452d99aeee2f
Ilya Kulikov <ilya-art@mail.ru>
2017-04-10 13:47:04 +0300

    Preparing for server

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,7 +1,6 @@
 import { SET_FILTER } from '../actions';
 
 function reducer(state = 'ALL', action) {
-  console.log('filterReducer.state', state);
   switch (action.type) {
     case SET_FILTER:
       return action.filter;
