66087859be0b4c5d0d370d68886e9b730069adce
Ilya Kulikov <ilya-art@mail.ru>
2017-04-10 12:24:03 +0300

    Separate store

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import reducer from './reducers';
-import state from './state';
+import store from './store';
 import App from './App';
 import { handleError } from './actions';
-
-const store = createStore(reducer, state);
-store.subscribe(() => console.log(store.getState()));
-store.dispatch(handleError('404'));
 
 ReactDOM.render(
   <Provider store={store}>
